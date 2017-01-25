72f09aea708aa75f4ba1bed3f1007fe25d602ac1
Tyler Gaugler <tylergaugler16@gmail.com>
2017-01-25 12:41:52 -0500

    add modal popup for event with weather info

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,86 @@
 $(document).ready(function() {
 
 
+  city = $('[name="city"]').val() || 'new york';
+
+  //ajax call to get weather of next 15 days
+  // store the result in global array weatherDates[]
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/forecast/daily',
+    data: {
+      APPID: 'beaace900c34ef2575c7c9a2fa4f8785',
+      q: city,
+      mode: 'json',
+      cnt: '15',
+      units: 'imperial'
+    },
+    success: function(result){
+      weatherDates = result;
+      // console.log(result.list[0]);
+      // $('.fc-today').append(result.list[0].rain);
+      var day = $('#calendar').fullCalendar('getDate');
+      for(var i= 0;i<result.list.length;i++){
+        m = day._d.getMonth()+1;
+        if(m < 10) m = "0"+m;
+        d = day._d.getDate();
+        if(d<10) d = "0"+d;
+        var dateString = +day._d.getUTCFullYear() +"-"+m+"-"+d;
+        // adds date to the returned object
+        result.list[i].date = dateString;
+        // $('[data-date="'+dateString+'"]').append("yeet");
+        // $('.fc-today').append(result.list[i].rain);
+        $('#calendar').fullCalendar( 'incrementDate', {days: 1} );
+        day = $('#calendar').fullCalendar('getDate');
+
+      }
+      //returns day back to current day
+      $('#calendar').fullCalendar('today');
+
+    }
+  });
+
+
+// creates calender
+// adds ajax call to dayClick functin to get weather for specific dayClick
+// then makes ajax call to check if day has any events
+// displays weather info and event info if they are available
 
     $('#calendar').fullCalendar({
       fixedWeekCount: false,
       dayClick: function(date, allDay, jsEvent, view) {
 
             $('[name="start"]').val(date.format());
+            var day;
 
+
+            var weatherInfo = {
+              'clouds': 'Not Available',
+              'rain': 'Not Available',
+              'max_temp': 'Not Available',
+              'min_temp': 'Not Available',
+              'description': '',
+              'date': date.format()
+            };
+            if(weatherDates.list.length > 0){
+              // if there are forcasts, find the forecast for the selected date
+              for(var i=0;i<weatherDates.list.length;i++){
+                if(weatherDates.list[i].date == date.format()){
+                  weatherInfo = {
+                    'clouds': weatherDates.list[i].clouds  || 'Not Available',
+                    'rain': weatherDates.list[i].rain  || 'Not Available',
+                    'max_temp': weatherDates.list[i].temp.max || 'Not Available',
+                    'min_temp': weatherDates.list[i].temp.min || 'Not Available',
+                    'description': weatherDates.list[i].weather[0].description || '',
+                    'date': date.format()
+                  };
+                }
+              }
+            }
+            else{
+
+
+            }
+            console.log(weatherInfo);
             // console.log(date);
             // send ajax request to see if day has event scheduled
             // then opens modal with event info
@@ -19,12 +92,23 @@ $(document).ready(function() {
                   var title= response.events[0].title;
                   var description = response.events[0].description;
                   Modal.open({
-                      content: '<h1>'+title+ '</h1><p>'+description+'</p>',
+                      content: '<h1>'+title+ '</h1><p>'+description+'</p>'+'<h3>High:</h3><span>'+weatherInfo.max_temp+'F</span><h3>Low:</h3><span>'+weatherInfo.min_temp+'F</span><h3>Chance of Rain:</h3><span>'+weatherInfo.rain+'</span><h3>Cloud Cover:</h3><span>'+weatherInfo.clouds+'</span><p>'+weatherInfo.description+'</p><h3 id="modalDate">'+weatherInfo.date+'</h3>',
                       width: '50%', // Can be set to px, em, %, or whatever else is out there.
                       height: '60%',
                       hideclose: true, // Hides the close-modal graphic
                       // closeAfter: 10
                     });
+                }
+                // if there is no event for that day, just dipslay the weather for that day if possible
+                else{
+                  Modal.open({
+                      content: '<h1>No Event Scheduled for This Date</h1>'+'<h3>High:</h3><span>'+weatherInfo.max_temp+'F</span><h3>Low:</h3><span>'+weatherInfo.min_temp+'F</span><h3>Chance of Rain:</h3><span>'+weatherInfo.rain+'</span><h3>Cloud Cover:</h3><span>'+weatherInfo.clouds+'</span><p>'+weatherInfo.description+'</p><h3 id="modalDate">'+weatherInfo.date+'</h3>',
+                      width: '50%', // Can be set to px, em, %, or whatever else is out there.
+                      height: '60%',
+                      hideclose: true, // Hides the close-modal graphic
+                      // closeAfter: 10
+                    });
+
                 }
               }
 
@@ -37,6 +121,8 @@ $(document).ready(function() {
     // check if this day has an event before
 
 
+
+// adds events to calendar from db
     $.ajax({
       url: '/events',
       data: {
@@ -94,41 +180,7 @@ $('#addEventButton').click(function(e){
 });
 
 
-      $('#findWeatherButton').click(function(e){
-        e.preventDefault();
-        var city = $('[name="city"]').val();
-        $.ajax({
-          url: 'http://api.openweathermap.org/data/2.5/forecast/daily',
-          data: {
-            APPID: 'beaace900c34ef2575c7c9a2fa4f8785',
-            q: 'new york',
-            mode: 'json',
-            cnt: '15',
-            units: 'imperial'
-          },
-          success: function(result){
-            console.log(result.list[0]);
 
-
-            // $('.fc-today').append(result.list[0].rain);
-            var day = $('#calendar').fullCalendar('getDate');
-            for(var i= 0;i<result.list.length;i++){
-              m = day._d.getMonth()+1;
-              if(m < 10) m = "0"+m;
-              d = day._d.getDate();
-              if(d<10) d = "0"+d;
-              var dateString = +day._d.getUTCFullYear() +"-"+m+"-"+d;
-              console.log(dateString);
-              $('[data-date="'+dateString+'"]').append("yeet");
-              // $('.fc-today').append(result.list[i].rain);
-              $('#calendar').fullCalendar( 'incrementDate', {days: 1} );
-              day = $('#calendar').fullCalendar('getDate');
-
-            }
-
-          }
-        });
-      });
 
       $('#modal').click(function(){
         Modal.open({
